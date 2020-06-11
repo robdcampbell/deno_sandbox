5eacf5e2caa2e6ee1c46255e3d7f19ec7a5da307
robdcampbell <rdeancampbell@gmail.com>
2020-06-11 14:38:53 -0400

    Typescript datatypes declaring variables and objects

--- ts_practice/typescript.js
+++ ts_practice/typescript.js
@@ -1,3 +1,24 @@
+//TYPESCRIPT NOTES
+// FIRST EXAMPLE
 var sum = function (a, b) {
     return a + b;
 };
+var answer = sum(4, 5);
+console.log(answer);
+//boolean
+var isCool = true;
+// number
+var age = 56;
+// string
+var eyeColor = 'blue';
+var ageQuote = "I'm not old, I'm only " + age + ".";
+// arrays
+var pets = ['cat', 'dog', 'pig'];
+var pets2 = ['lion', 'dragon', 'lizard'];
+// object
+var wizard = {
+    a: 'John'
+};
+// null and undefined
+var meh = undefined;
+var nooo = null;
--- ts_practice/typescript.ts
+++ ts_practice/typescript.ts
@@ -1,3 +1,34 @@
+//TYPESCRIPT NOTES
+
+
+// FIRST EXAMPLE
 const sum = (a: number, b: number) => {
     return a+b;
 }
+
+const answer = sum(4,5)
+
+console.log(answer);
+
+//boolean
+let isCool: boolean =  true;
+
+// number
+let age: number = 56;
+
+// string
+let eyeColor: string = 'blue';
+let ageQuote: string = `I'm not old, I'm only ${age}.`;
+
+// arrays
+let pets: string[] = ['cat','dog','pig'];
+let pets2: Array<string> = ['lion','dragon','lizard'];
+
+// object
+let wizard: object = {
+    a: 'John'
+}
+
+// null and undefined
+let meh: undefined = undefined;
+let nooo: null = null;
