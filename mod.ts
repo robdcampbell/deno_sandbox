be3eeba4c7f25e119aa173e523da9377af2d8cbd
robdcampbell <rdeancampbell@gmail.com>
2020-06-13 21:43:16 -0400

    Deno Standard Library and Runtiem API practice

---
+++ mod.ts
@@ -0,0 +1,12 @@
+import { join } from "https://deno.land/std/path/mod.ts"
+
+
+async function readFile(){
+    const path = join("text_files","hello.txt")
+    const data = await Deno.readTextFile(path);
+    console.log(data);
+
+    
+}
+
+ await readFile();
